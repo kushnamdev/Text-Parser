888f5b10ed68ad0620354769fb6904013d38f2d1
I-man <kushnamdev23@gmail.com>
2019-10-06 04:45:26 +0530

    First commit

---
+++ mysite/views.py
@@ -0,0 +1,100 @@
+# I have created this website - kush
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+
+
+def analyze(request):
+    # Get the text
+    global params
+    djtext = request.POST.get('text', 'default')
+
+    #Check Checkbox Values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    titlecase = request.POST.get('camelcase', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
+    lowercase = request.POST.get('lowercase', 'off')
+
+
+
+    # check which check box is on
+    if removepunc == 'on':
+        punctations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+
+
+
+    if fullcaps == 'on':
+
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'changed to UPPERCASE  ', 'analyzed_text': analyzed}
+
+        djtext = analyzed
+
+    if lowercase == 'on':
+
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.lower()
+        params = {'purpose': 'changed to lowercase ', 'analyzed_text': analyzed}
+        djtext = analyzed
+    if charcount == 'on':
+
+        analyzed = ""
+        for char in djtext:
+            analyzed = len(djtext)
+        params = {'purpose': 'characters counter', 'analyzed_text': analyzed}
+
+
+        djtext = analyzed
+    if titlecase == "on":
+
+        analyzed = ""
+
+        for char in djtext:
+
+
+            analyzed = analyzed + char.title()
+
+
+        params = {'purpose': 'Changed to Titlecase', 'analyzed_text': analyzed}
+
+
+        djtext = analyzed
+    if (extraspaceremover == "on"):
+        analyzed = ""
+        for Index, char in enumerate(djtext):
+            if not (djtext[Index] == " " and djtext[Index + 1] == " "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+
+    if removepunc != 'on' and fullcaps != 'on' and extraspaceremover != 'on' and lowercase!= 'on' and titlecase!= 'on' and charcount!= 'on':
+        return HttpResponse(" Please enable the checkboxes and try again :)")
+
+    return render(request, 'analyze.html', params)
+
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
---
+++ templates/contact.html
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Text parser</title>
+
+  </head>
+  <body style="background-color:white">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">TextParser.in</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item ">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span> </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about/">About us <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Contact us <span class="sr-only">(current)</span></a>
+      </li>
+
+    </ul>
+  </div>
+    <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form >
+
+</nav>
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Welcome to TextParser's</strong> You can contact me directly just by clicking the links below :)
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<div style="text-align:centre" class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-dark"><a href="https://www.instagram.com/itz_kushhh__/"> Instagram</a></button>
+  <button type="button" class="btn btn-dark"><a href="https://www.facebook.com/profile.php?id=100011186205641">Facebook<a/></button>
+  <button type="button" class="btn btn-dark"><a href="https://www.youtube.com/kushnamdev">Youtube</a></button>
+  <button type="button" class="btn btn-dark"><a href="https://github.com/kushnamdev"> Github</a></button>
+</div>
+<h3><strong>Note:</strong> You can also get in touch with me on Gmail,It is my Gmail - kushnamdev23@gmail.com</h3>
